b0b635e3e55ce758851fef5f30ba0f915564ad11
JuanGutiierrez <joselitoguti2003@gmail.com>
2023-10-10 06:24:46 -0500

    feat: creamos menu de onixapp en el front

---
+++ data/empleados.py
@@ -0,0 +1,18 @@
+empleado1={
+    "id":1,
+    "nombre":"Juan Jose",
+    "apellidos":"Gallego Mesa",
+    "salariomensual":15000000,
+    "deudas":True,
+    "retencionFuente":0.1,
+    "correo":"jjme@grupouribe.com",
+    "telefono":"3225962363",
+    "cargo":"Arquitecto"
+}
+
+# desarrollador junior 
+# desarrollador intermedio 
+# desarrollador avanzado  
+# arquitecto
+
+#SOLO si se ganan mas de 6 millones se aplica retencion en la fuente del 10%
